299e40dba532252fa61de59f264fbcb86eeb7bdf
Archana Jena <142888938+archanaJena@users.noreply.github.com>
2023-08-22 17:52:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function redirect() {
+  location.href = "index2.html";
+}
+function redirect1() {
+  location.href = "index3.html";
+}
+function redirect2() {
+  location.href = "index4.html";
+}
+function redirect4() {
+  location.href = "index.html";
+}
+function redirect3() {
+  location.href = "index5.html";
+}
